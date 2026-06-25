51ae14abe4df6a388e5cceed158758edd65ee753
Twanano Rikhotso <ntwananorikhotso05@gmail.com>
2026-06-25 13:09:19 +0200

    clean column names and created remote percentage

--- ingest_metrics.py
+++ ingest_metrics.py
@@ -11,3 +11,21 @@ print(jobs_df.head())
 
 merged_df = pd.merge(loadshedding_df, jobs_df, on="date")  #Merging data
 
+# clean column names
+merged_df = merged_df.rename(columns={
+    "date": "log_date",
+    "stage": "loadshedding_stage"
+})
+
+
+# Remote Percentage
+merged_df["remote_percentage"] = (
+    merged_df["remote_jobs"] / merged_df["total_jobs"]
+) * 100
+
+merged_df["remote_percentage"] = merged_df["remote_percentage"].round(2)
+
+
+# if total jobs = 0, I might get division errors
+# Will fix that later
+
